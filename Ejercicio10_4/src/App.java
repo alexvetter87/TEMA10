0b944789e04f2b76090e4c8f793ffa5b74d8a93c
alexvetter87 <124079579+alexvetter87@users.noreply.github.com>
2023-04-18 19:35:33 +0200

    Add files via upload

---
+++ Ejercicio10_4/src/App.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader("C:\\Users\\JAVA\\JAVA\\TEMA 10\\Ejercicio10_4\\src\\NumerosReales.txt"));
+            String texto = in.readLine();
+            String[] subcadenas = texto.split("");
+            double suma = 0;
+            for (int i = 0; i < subcadenas.length; i++) {
+                suma += Double.valueOf(subcadenas[i]);
+            }
+            System.out.println("suma:" + suma + "\tmedia:" + suma / subcadenas.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                    ;
+                }
+
+            }
+        }
+    }
+
+}
