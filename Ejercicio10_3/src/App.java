cc0fa5e76f811bf10c715ef57956f8567dfd1683
alexvetter87 <124079579+alexvetter87@users.noreply.github.com>
2023-04-18 17:24:28 +0200

    Add files via upload

---
+++ Ejercicio10_3/src/App.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String texto = "";
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader ("Main.java"));
+            String linea = in.readLine();
+            while (linea != null) {
+                texto = texto + linea + '\n';
+             linea = in.readLine();
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }finally{
+            if (in!=null) {
+                try{
+                    in.close();
+                }catch (IOException ex  ){
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+               System.out.println(texto);
+}
