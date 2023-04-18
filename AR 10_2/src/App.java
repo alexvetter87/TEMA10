2d197366c9202423306791b53f1ac2a7d3b6e4fd
alexvetter87 <124079579+alexvetter87@users.noreply.github.com>
2023-04-18 16:59:17 +0200

    Add files via upload

--- AR 10_2/src/App.java
+++ AR 10_2/src/App.java
@@ -1,5 +1,30 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String texto="";
+        FileReader in = null;
+        try{
+            in = new FileReader("Main.java");
+            int c= in.read();
+            while (c!=-1) {
+                texto = texto + (char) c;
+                c=in.read();
+            }
+ 
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }finally {
+            if (in!=null) {
+                try {
+                    in.close();
+                }catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+        System.out.println(texto);
     }
 }
